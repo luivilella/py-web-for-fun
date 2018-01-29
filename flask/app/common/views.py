4700dc557b10b7c51b64ee83ea1dab5c9f305add
Luis Vilella <luivilella@gmail.com>
2018-01-28 20:16:06 -0800

    chore(flask4fun): stop hardcoding HTTP status code     - Add a HTTP status code map available for all views that uses base view

--- flask/app/common/views.py
+++ flask/app/common/views.py
@@ -1,8 +1,11 @@
+from http import HTTPStatus
 from flask.views import MethodView
 from flask import jsonify
 
 
 class BaseView(MethodView):
 
-    def json_response(self, _dict, status_code=200):
+    HTTP = HTTPStatus
+
+    def json_response(self, _dict, status_code=HTTPStatus.OK):
         return jsonify(_dict), status_code

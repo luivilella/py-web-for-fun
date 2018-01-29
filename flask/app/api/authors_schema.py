e6c4adf175ff648d3b928a89016a54676ba70ab3
Luis Vilella <luivilella@gmail.com>
2018-01-28 18:23:35 -0800

    feat(flask4fun): expose new fields to serializer     - Explicit fields in the serializer     - Block read-only fields `dump_only`     - Make an example of value generated by a method

--- flask/app/api/authors_schema.py
+++ flask/app/api/authors_schema.py
@@ -1,10 +1,30 @@
+from marshmallow import fields
 from .. import ma
 from ..models.authors import Author
 
 
 class AuthorSchema(ma.ModelSchema):
+
+    name = fields.Str()
+    email = fields.Email()
+    created_at = fields.DateTime(dump_only=True)
+    total_books = fields.Int(dump_only=True)
+    last_book = fields.Method('get_last_book', dump_only=True)
+
+    def get_last_book(self, author):
+        book = author.last_book()
+        return getattr(book, 'title', None)
+
     class Meta:
         model = Author
+        fields = (
+            'id',
+            'created_at',
+            'name',
+            'email',
+            'total_books',
+            'last_book'
+        )
 
 
 author_schema = AuthorSchema()
